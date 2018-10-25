18b5140f47c94b6ba48f948646e85efbdc77529d
Azee <aziztitu1996@gmail.com>
2018-10-25 02:40:26 -0500

    Adds Command Interface

---
+++ angular-app/src/app/shared/components/command-interface/command-interface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-command-interface',
+  templateUrl: './command-interface.component.html',
+  styleUrls: ['./command-interface.component.scss']
+})
+export class CommandInterfaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular-app/src/app/shared/shared.module.ts
+++ angular-app/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommandInterfaceComponent } from './components/command-interface/command-interface.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CommandInterfaceComponent]
 })
 export class SharedModule { }
