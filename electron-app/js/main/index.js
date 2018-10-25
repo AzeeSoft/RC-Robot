13418c8a70430de0e26636699b009d06e15a7cc0
Azee <aziztitu1996@gmail.com>
2018-10-24 23:26:28 -0500

    Ignores the compiled electron js files (electron-app/js) from git

--- electron-app/js/main/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppInstance_1 = require("./core/app/AppInstance");
-let appInstance = AppInstance_1.AppInstance.getInstance();
-appInstance.start();
